c5cdc6ba0bd1d4e4c5be7dfaf5eb015737f9c8fa
amoghmc <amoghmc@github.com>
2023-08-03 23:48:01 +0530

    solve add and sub

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,11 +9,41 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    try:
+        # Get JSON data from the request
+        json_data = request.get_json()
+
+        # Access JSON values using keys
+        first_no = int(json_data['first'])
+        second_no = int(json_data['second'])
+        result = first_no + second_no
+
+        # Return a response
+        return jsonify({'result: ' + str(result)}), 200
+
+    except KeyError:
+        return jsonify({'error': 'Invalid JSON data or missing fields.'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    try:
+        # Get JSON data from the request
+        json_data = request.get_json()
+
+        # Access JSON values using keys
+        first_no = int(json_data['first'])
+        second_no = int(json_data['second'])
+        result = first_no - second_no
+
+        # Return a response
+        return jsonify({'result: ' + str(result)}), 200
+
+    except KeyError:
+        return jsonify({'error': 'Invalid JSON data or missing fields.'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
