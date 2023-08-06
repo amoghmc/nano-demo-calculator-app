6f5cb16eca749412bcb40f2d3fb06d67298e60bc
amoghmc <amoghmc@github.com>
2023-08-06 13:23:48 +0530

    refactor

--- server.py
+++ server.py
@@ -10,42 +10,30 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-	try:
-		# Get JSON data from the request
-		json_data = request.get_json()
+	# Get JSON data from the request
+	json_data = request.get_json()
 
-		# Access JSON values using keys
-		first_no = int(json_data['first'])
-		second_no = int(json_data['second'])
-		result = first_no + second_no
+	# Access JSON values using keys
+	first_no = int(json_data['first'])
+	second_no = int(json_data['second'])
+	result = first_no + second_no
 
-		# Return a response
-		return jsonify({'result': result}), 200
-
-	except KeyError:
-		return jsonify({'error': 'Invalid JSON data or missing fields.'}), 400
-	except Exception as e:
-		return jsonify({'error': str(e)}), 500
+	# Return a response
+	return jsonify({'result': result})
 
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-	try:
-		# Get JSON data from the request
-		json_data = request.get_json()
+	# Get JSON data from the request
+	json_data = request.get_json()
 
-		# Access JSON values using keys
-		first_no = int(json_data['first'])
-		second_no = int(json_data['second'])
-		result = first_no - second_no
+	# Access JSON values using keys
+	first_no = int(json_data['first'])
+	second_no = int(json_data['second'])
+	result = first_no - second_no
 
-		# Return a response
-		return jsonify({'result': result}), 200
-
-	except KeyError:
-		return jsonify({'error': 'Invalid JSON data or missing fields.'}), 400
-	except Exception as e:
-		return jsonify({'error': str(e)}), 500
+	# Return a response
+	return jsonify({'result': result})
 
 
 if __name__ == '__main__':
